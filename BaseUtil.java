7383e39b791689be6eec0c5ea541d1e78a0e3913
vinayatekale <39888041+vinayatekale@users.noreply.github.com>
2019-12-02 02:17:06 +0000

    Add files via upload

---
+++ BaseUtil.java
@@ -0,0 +1,13 @@
+package Base;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+
+
+public class BaseUtil {
+
+    public WebDriver Driver;
+
+}
